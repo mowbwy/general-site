c8bf3970875afd811790b50dc5beb254fd9a0dcb
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-10 13:41:19 -0700

    Testing..

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -54,10 +54,10 @@ export default function Home() {
   return (
     <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
       <h1>Home</h1>
-      <Link to="/hero">View Hero Section</Link><br />
+      <Link to="/Hero">View Hero Section</Link><br />
       <Link to="/about">View the About section</Link><br />
       <Link to="/contact">View Contact Information</Link><br />
-      <Link to="/projects">View Projects</Link><br />
+      <Link to="/Projects">View Projects</Link><br />
       <div style={{ marginTop: "30px", marginBottom: "20px" }}>
         <div style={{ display: "flex", gap: "8px", marginBottom: "15px" }}>
           <input
