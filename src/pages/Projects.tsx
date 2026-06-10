1c3b66aeb1ba6a8b2be7c1179163d30e540bf375
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-10 12:59:24 -0700

    Updated other sections.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -13,6 +13,8 @@ export default function Users() {
   return (
     <div>
       <h1>Projects</h1><br/>
+      <p>This is the projects section. I will place all my projects that have to do with
+        computer science here. I am not working in any projects at the moment.</p>
       <Link to="/">Back Home</Link>
       {users.map((u: any) => (
         <p key={u._id}>{u.name}</p>
