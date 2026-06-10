8be2ab87353d7611469b4c197128b35d44e3d6e7
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-10 16:48:42 -0700

    Not a jr.

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -6,7 +6,7 @@ export default function About() {
     <div>
       <h1>Hero Page</h1>
       <p>Hello, my name is Joseph and I'm a computer science student. My current
-        degree is a B.S. in Computer Science and I am currently a junior. I have experience in Java, Python, and web development. I have also done some work with machine learning and data science. I am passionate about technology and I am always looking for new opportunities to learn and grow.
+        degree is a B.S. in Computer Science. I have experience in Java, Python, and web development. I have also done some work with machine learning and data science. I am passionate about technology and I am always looking for new opportunities to learn and grow.
       I am currently pursuing my degree at South New Hampshire University so that I could get a better education.</p>
       <Link to="/">Back Home</Link>
     </div>
