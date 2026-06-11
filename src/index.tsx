588358194ca939bd0323fff6640d860e71530d58
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-10 17:12:48 -0700

    Try it out

--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Hero from "./pages/Hero";
 import Home from "./pages/home";
 import Projects from "./pages/Projects";
-export async function searchItems(query: string) {
-  const res = await fetch(`http://localhost:5000/api/search?q=${query}`);
-  return res.json();
-}
 
 const router = createBrowserRouter(
   [
-    {
-      path: "",
-      element: <Home />,
-    },
-    {
-      path: "home",
-      element: <Home />,
-    },
-    {
-      path: "about",
-      element: <About />,
-    },
-    {
-      path: "contact",
-      element: <Contact />,
-    },
-    {
-      path: "Hero",
-      element: <Hero />,
-    },
-    {
-      path: "Projects",
-      element: <Projects />,
-    },
+    { path: "/", element: <App /> },
+    { path: "about", element: <About /> },
+    { path: "contact", element: <Contact /> },
+    { path: "hero", element: <Hero /> },
+    { path: "home", element: <Home /> },
+    { path: "projects", element: <Projects /> },
   ],
   {
-    basename: "/",
+    basename: "/general-site",
   }
 );
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);  
+);
