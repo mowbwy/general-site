c86e7bb402a3206571158aba5645064d3e0e32c8
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-10 20:02:25 -0700

    Updated index page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Index</h1>
       <Link to="/about">Go to About</Link><br />
       <Link to="/contact">Go to Contact</Link><br />
       <Link to="/Search">Go to Search</Link><br />
