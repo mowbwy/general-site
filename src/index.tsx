42b0f04a5d1f1a6601fa71d0782ad0eb6e73de7d
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-10 17:23:19 -0700

    Minor touches

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Hero from "./pages/Hero";
-import Home from "./pages/Search";
+import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 
 const router = createBrowserRouter(
@@ -14,7 +14,7 @@ const router = createBrowserRouter(
     { path: "about", element: <About /> },
     { path: "contact", element: <Contact /> },
     { path: "hero", element: <Hero /> },
-    { path: "search", element: <search /> },
+    { path: "home", element: <Home /> },
     { path: "projects", element: <Projects /> },
   ],
   {
