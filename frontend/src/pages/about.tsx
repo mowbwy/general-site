aba1283edc566e46626818da27f79ebbb9e2fc95
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-18 16:56:05 -0700

    Update?

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function About() {
+export default function about() {
   return (
     <section className="about-section">
       <h2 className="about-title">About Me</h2>
