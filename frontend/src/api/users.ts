3a8e5bdcff940284522ba737678372857d94f19f
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-23 10:22:39 -0700

    Updated link

--- frontend/src/api/users.ts
+++ frontend/src/api/users.ts
@@ -1,4 +1,4 @@
 export async function getUsers() {
-  const res = await fetch("http://localhost:5000/api/users");
+  const res = await fetch("../backend/users.pyhttps://github.com/mowbwy/general-site/blob/8a0dfae55654812d6014881a40cc311ce7b49704/backend/users.py");
   return res.json();
 }
