8a0dfae55654812d6014881a40cc311ce7b49704
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-23 10:09:26 -0700

    Updated users

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from backend.users import router
+from users import router
 
 app = FastAPI()
 
