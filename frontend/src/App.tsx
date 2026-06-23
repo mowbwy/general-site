bd45b4ed5677f54c7e65d10f24c1b9f6419843fb
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-22 22:02:08 -0700

    Updated login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,8 @@ export default function App() {
 
     const apiUrl = import.meta.env.VITE_API_URL;
 
-    fetch(`https://general-site-production.up.railway.app/hello`)
+    fetch(`fetch("https://general-site-production.up.railway.app/api/hello")
+`)
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch((err) => {
