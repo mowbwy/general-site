a7063d718dbb3d439d01db0219a1d047350fe339
@mowbwy <jalvayero2@toromail.csudh.edu>
2026-06-22 22:34:38 -0700

    Updated profile pic

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -14,7 +14,7 @@ export default function About() {
 
       <div className="about-content">
         <img
-          src="/general-site/profile.jpg"
+          src="https://mowbwy.github.io/general-site/profile.jpg"
           alt="Profile"
           className="profile-pic"
         />
